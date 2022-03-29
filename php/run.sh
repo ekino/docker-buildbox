992acb3a4b8525a610c1126d18b4b312b5f5a8b9
swve <bdswve@gmail.com>
2022-03-29 18:19:50 +0200

    ⚡️ change install method

--- php/run.sh
+++ php/run.sh
@@ -75,37 +75,14 @@ echo "Done PHP!"
 
 # AWS Tools
 # install glibc & aws compatibility for alpine
-echo "Starting AWS"
+echo "Installing AWS"
 export GLIBC_VER=2.31-r0
 
 # install glibc compatibility for alpine
-apk --no-cache add \
-binutils \
-curl \
-&& curl -sL https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /etc/apk/keys/sgerrand.rsa.pub \
-&& curl -sLO https://github.com/sgerrand/alpine-pkg-glibc/releases/download/$GLIBC_VER/glibc-$GLIBC_VER.apk \
-&& curl -sLO https://github.com/sgerrand/alpine-pkg-glibc/releases/download/$GLIBC_VER/glibc-bin-$GLIBC_VER.apk \
-&& curl -sLO https://github.com/sgerrand/alpine-pkg-glibc/releases/download/$GLIBC_VER/glibc-i18n-$GLIBC_VER.apk \
-&& apk add --no-cache \
-glibc-$GLIBC_VER.apk \
-glibc-bin-$GLIBC_VER.apk \
-glibc-i18n-$GLIBC_VER.apk \
-&& /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8 \
-&& curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip \
-&& unzip awscliv2.zip \
-&& aws/install \
-&& rm -rf \
-awscliv2.zip \
-aws \
-/usr/local/aws-cli/v2/current/dist/aws_completer \
-/usr/local/aws-cli/v2/current/dist/awscli/data/ac.index \
-/usr/local/aws-cli/v2/current/dist/awscli/examples \
-glibc-*.apk \
-&& find /usr/local/aws-cli/v2/current/dist/awscli/botocore/data -name examples-1.json -delete \
-&& apk --no-cache del \
-binutils \
-curl \
-&& rm -rf /var/cache/apk/*
+apk add -q --no-cache bash build-base ca-certificates curl gettext git libffi-dev linux-headers musl-dev openldap-dev openssh-client python3-dev gcc libffi-dev libressl-dev make rsync tzdata groff zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 echo "Done AWS!"
 
 echo "Adding an up to date mime-types definition file"
