c045f0b8af1c36cfa8444ebe55280406f7f72449
bonnai.djicko <@ekino.com>
2022-05-04 21:17:52 +0200

    Bitcoind: Fix multi-arch images

--- bitcoind/run.sh
+++ bitcoind/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 if [ "${TARGETARCH}" = "arm64" ]; then
-    BITCOIN_URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/bitcoin-${BITCOIN_VERSION}-arm-linux-gnueabihf.tar.gz"
+    BITCOIN_URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/bitcoin-${BITCOIN_VERSION}-aarch64-linux-gnu.tar.gz"
     BITCOIN_SHA256="${SHA256_ARM64}"
 else
     BITCOIN_URL="https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_VERSION}/bitcoin-${BITCOIN_VERSION}-x86_64-linux-gnu.tar.gz"
