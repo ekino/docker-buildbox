54c8b360e36a2f7c48c9c43f256bc4b875536992
bonnai.djicko <@ekino.com>
2022-06-23 10:36:12 +0200

    Build arm and amd images for ansible, aws, bitcoind, dind, golang

--- dind/run.sh
+++ dind/run.sh
@@ -2,17 +2,11 @@
 
 set -ex
 
-if [ "${TARGETARCH}" = "arm64" ]; then
-    TRIVY_ARCH="Linux-ARM64"
-else
-    TRIVY_ARCH="Linux-64bit"
-fi
-
 echo "Install AWS & Azure CLIs..." &&
     apk add -q --no-cache bash build-base ca-certificates curl gettext git libffi-dev linux-headers musl-dev openldap-dev openssh-client python3-dev gcc libffi-dev libressl-dev make rsync tzdata groff &&
     python3 -m ensurepip &&
     pip3 install --no-cache --upgrade pip setuptools wheel &&
-    pip -q install awscli boto3 PyYAML &&
+    pip -q install awscliv2 awscli boto3 PyYAML &&
     pip install -r requirements.txt &&
     echo "Done installing AWS & Azure CLIs"
 
@@ -21,14 +15,7 @@ echo "Install Taskfile..." &&
     echo "Done Install Taskfile"
 
 echo "Install Docker Compose..." &&
-    apk del libc6-compat &&
-    curl -sSL https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /etc/apk/keys/sgerrand.rsa.pub &&
-    curl -sSL https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk -O &&
-    curl -sSL https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-bin-${GLIBC_VERSION}.apk -O &&
-    apk add --update -q glibc-${GLIBC_VERSION}.apk glibc-bin-${GLIBC_VERSION}.apk &&
-    rm /etc/apk/keys/sgerrand.rsa.pub glibc-${GLIBC_VERSION}.apk glibc-bin-${GLIBC_VERSION}.apk &&
-    curl -sSL https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose &&
-    chmod +x /usr/local/bin/docker-compose &&
+    apk add --update docker-compose &&
     docker-compose --version &&
     echo "Done install Docker Compose"
 
--- golang/run.sh
+++ golang/run.sh
@@ -4,84 +4,65 @@ set -ex
 
 echo "Starting...\n"
 
-echo "Installing packages..."
-apt-get update -q
-apt-get -qq -y install rsync zip
-echo "Successfully installed packages\n"
+echo "Installing AWS CLI..." &&
+    apt-get update -qq && apt-get install -qq -y curl groff-base python3-pip rsync zip &&
+    python3 -m ensurepip &&
+    pip3 install --no-cache --upgrade pip setuptools wheel &&
+    curl ${AWSCLI_URL} -o "awscliv2.zip" &&
+    unzip awscliv2.zip &&
+    ./aws/install &&
+    echo "Successfully installed AWS CLI\n"
 
-echo "Installing AWS CLI..."
-apt-get -qq -y install groff-base python3-pip  && \
-python3 -m ensurepip && \
-pip3 install --no-cache --upgrade pip setuptools wheel && \
-pip install -q -U awscli && \
-echo "Successfully installed AWS CLI\n"
+echo "Adding an up to date mime-types definition file" &&
+    curl -sSL https://salsa.debian.org/debian/mime-support/raw/master/mime.types -o /etc/mime.types
 
-echo "Adding an up to date mime-types definition file"
-curl -sSL https://salsa.debian.org/debian/mime-support/raw/master/mime.types -o /etc/mime.types
+echo "Installing gitleaks..." &&
+    wget -q https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks-${GITLEAKS_ARCH} &&
+    mv gitleaks-${GITLEAKS_ARCH} /usr/local/bin/gitleaks && chmod +x /usr/local/bin/gitleaks &&
+    echo "Successfully installed gitleaks\n"
 
-echo "Installing gitleaks..."
-if [ "${TARGETARCH}" = "arm64" ]; then
-    wget -q https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks-linux-arm
-    mv gitleaks-linux-arm /usr/local/bin/gitleaks && chmod +x /usr/local/bin/gitleaks
-else
-    wget -q https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks-linux-amd64
-    mv gitleaks-linux-amd64 /usr/local/bin/gitleaks && chmod +x /usr/local/bin/gitleaks
-fi
-echo "Successfully installed gitleaks\n"
+echo "Installing golangci-lint..." &&
+    wget -q https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCILINT_VERSION}/golangci-lint-${GOLANGCILINT_VERSION}-linux-${TARGETARCH}.tar.gz &&
+    tar -xzf golangci-lint-${GOLANGCILINT_VERSION}-linux-${TARGETARCH}.tar.gz &&
+    mv golangci-lint-${GOLANGCILINT_VERSION}-linux-${TARGETARCH}/golangci-lint /usr/local/bin &&
+    rm -rf golangci-lint* &&
+    echo "Successfully installed golangci-lint\n"
 
-echo "Installing golangci-lint..."
-wget -q https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCILINT_VERSION}/golangci-lint-${GOLANGCILINT_VERSION}-linux-${TARGETARCH}.tar.gz
-tar -xzf golangci-lint-${GOLANGCILINT_VERSION}-linux-${TARGETARCH}.tar.gz
-mv golangci-lint-${GOLANGCILINT_VERSION}-linux-${TARGETARCH}/golangci-lint /usr/local/bin
-rm -rf golangci-lint*
-echo "Successfully installed golangci-lint\n"
+echo "Installing go-mod-upgrade..." &&
+    wget -q https://github.com/oligot/go-mod-upgrade/releases/download/v${GOMODUPGRADE_VERSION}/go-mod-upgrade_${GOMODUPGRADE_VERSION}_${GOMODUPGRADE_ARCH}.tar.gz &&
+    tar -xzf go-mod-upgrade_${GOMODUPGRADE_VERSION}_${GOMODUPGRADE_ARCH}.tar.gz -C /usr/local/bin && rm go-mod-upgrade* &&
+    echo "Successfully installed go-mod-upgrade\n"
 
-echo "Installing go-mod-upgrade..."
-if [ "${TARGETARCH}" = "arm64" ]; then
-    wget -q https://github.com/oligot/go-mod-upgrade/releases/download/v${GOMODUPGRADE_VERSION}/go-mod-upgrade_${GOMODUPGRADE_VERSION}_Linux_arm64.tar.gz
-    tar -xzf go-mod-upgrade_${GOMODUPGRADE_VERSION}_Linux_arm64.tar.gz -C /usr/local/bin && rm go-mod-upgrade*
-else
-    wget -q https://github.com/oligot/go-mod-upgrade/releases/download/v${GOMODUPGRADE_VERSION}/go-mod-upgrade_${GOMODUPGRADE_VERSION}_Linux_x86_64.tar.gz
-    tar -xzf go-mod-upgrade_${GOMODUPGRADE_VERSION}_Linux_x86_64.tar.gz -C /usr/local/bin && rm go-mod-upgrade*
-fi
-echo "Successfully installed go-mod-upgrade\n"
+echo "Installing go-swagger..." &&
+    wget -q https://github.com/go-swagger/go-swagger/releases/download/v${GOSWAGGER_VERSION}/swagger_linux_${TARGETARCH} &&
+    mv swagger_linux_${TARGETARCH} /usr/local/bin/swagger && chmod +x /usr/local/bin/swagger &&
+    echo "Successfully installed go-swagger\n"
 
-echo "Installing go-swagger..."
-wget -q https://github.com/go-swagger/go-swagger/releases/download/v${GOSWAGGER_VERSION}/swagger_linux_${TARGETARCH}
-mv swagger_linux_${TARGETARCH} /usr/local/bin/swagger && chmod +x /usr/local/bin/swagger
-echo "Successfully installed go-swagger\n"
+echo "Installing migrate..." &&
+    wget -q https://github.com/golang-migrate/migrate/releases/download/v${MIGRATE_VERSION}/migrate.linux-${TARGETARCH}.tar.gz &&
+    tar -xzf migrate.linux-${TARGETARCH}.tar.gz -C /usr/local/bin && mv /usr/local/bin/migrate.linux-${TARGETARCH} /usr/local/bin/migrate &&
+    rm migrate.linux-${TARGETARCH}.tar.gz &&
+    echo "Successfully installed migrate\n"
 
-echo "Installing migrate..."
-wget -q https://github.com/golang-migrate/migrate/releases/download/v${MIGRATE_VERSION}/migrate.linux-${TARGETARCH}.tar.gz
-tar -xzf migrate.linux-${TARGETARCH}.tar.gz -C /usr/local/bin && mv /usr/local/bin/migrate.linux-${TARGETARCH} /usr/local/bin/migrate
-rm migrate.linux-${TARGETARCH}.tar.gz
-echo "Successfully installed migrate\n"
+echo "Installing mockgen..." &&
+    wget -q https://github.com/golang/mock/releases/download/v${MOCKGEN_VERSION}/mock_${MOCKGEN_VERSION}_linux_${TARGETARCH}.tar.gz &&
+    tar -xzf mock_${MOCKGEN_VERSION}_linux_${TARGETARCH}.tar.gz &&
+    mv mock_${MOCKGEN_VERSION}_linux_${TARGETARCH}/mockgen /usr/local/bin &&
+    rm -rf mock* &&
+    echo "Successfully installed mockgen\n"
 
-echo "Installing mockgen..."
-wget -q https://github.com/golang/mock/releases/download/v${MOCKGEN_VERSION}/mock_${MOCKGEN_VERSION}_linux_${TARGETARCH}.tar.gz
-tar -xzf mock_${MOCKGEN_VERSION}_linux_${TARGETARCH}.tar.gz
-mv mock_${MOCKGEN_VERSION}_linux_${TARGETARCH}/mockgen /usr/local/bin
-rm -rf mock*
-echo "Successfully installed mockgen\n"
+echo "Installing modd..." &&
+    wget -q ${MOOD_URL} &&
+    tar -xzf modd-${MODD_VERSION}-${MOOD_ARCH}.tgz && mv modd-${MODD_VERSION}-linuxARM/modd /usr/local/bin &&
+    rm -rf modd-${MODD_VERSION}-${MOOD_ARCH}* &&
+    echo "Successfully installed modd\n"
 
-echo "Installing modd..."
-if [ "${TARGETARCH}" = "arm64" ]; then
-    wget -q https://github.com/cortesi/modd/releases/download/v${MODD_VERSION}/modd-${MODD_VERSION}-linuxARM.tgz
-    tar -xzf modd-${MODD_VERSION}-linuxARM.tgz && mv modd-${MODD_VERSION}-linuxARM/modd /usr/local/bin
-    rm -rf modd-${MODD_VERSION}-linuxARM*
-else
-    wget -q https://github.com/cortesi/modd/releases/download/v${MODD_VERSION}/modd-${MODD_VERSION}-linux64.tgz
-    tar -xzf modd-${MODD_VERSION}-linux64.tgz && mv modd-${MODD_VERSION}-linux64/modd /usr/local/bin
-    rm -rf modd-${MODD_VERSION}-linux64*
-fi
-echo "Successfully installed modd\n"
+echo "Installing testfixtures..." &&
+    wget -q ${TESTFIXTURES_URL} &&
+    tar -xzf testfixtures_${TESTFIXTURES_ARCH}.tar.gz -C /usr/local/bin &&
+    rm testfixtures_${TESTFIXTURES_ARCH}.tar.gz &&
+    echo "Successfully installed testfixtures\n"
 
-echo "Installing testfixtures..."
-wget -q https://github.com/go-testfixtures/testfixtures/releases/download/v${TESTFIXTURES_VERSION}/testfixtures_linux_amd64.tar.gz
-tar -xzf testfixtures_linux_amd64.tar.gz -C /usr/local/bin
-rm testfixtures_linux_amd64.tar.gz
-echo "Successfully installed testfixtures\n"
+#go get golang.org/x/tools/cmd/goimports github.com/shuLhan/go-bindata/... github.com/boumenot/gocover-cobertura
 
-go get golang.org/x/tools/cmd/goimports github.com/shuLhan/go-bindata/... github.com/boumenot/gocover-cobertura
-
-echo "Done!";
+echo "Done!"
