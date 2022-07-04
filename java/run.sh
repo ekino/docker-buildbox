232bc55c1b1c623f97cc12c5c2053c9b5b8b9daf
bonnai.djicko <@ekino.com>
2022-07-04 18:53:55 +0200

    [WIP] CI : Fix test build and test jobs errors

--- java/run.sh
+++ java/run.sh
@@ -4,34 +4,33 @@ set -ex
 
 echo "Starting ..."
 
-echo "Updating packages using sources :"
-cat /etc/apt/sources.list
-apt-get -qq clean -qq && apt-get -qq update
+echo "Updating packages using sources :" &&
+    cat /etc/apt/sources.list &&
+    apt-get -qq clean -qq && apt-get -qq update
 
-echo "Install base"
-apt-get -qq -y install build-essential curl git make openssh-client jq unzip groff less
-echo "Done Install base!"
+echo "Install base" &&
+    apt-get -qq -y install build-essential curl git make openssh-client jq unzip groff less &&
+    echo "Done Install base!"
 
-echo "Installing AWS CLIv2"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-echo "Done installing awscliv2!"
+echo "Installing AWS CLIv2" &&
+    curl ${AWSCLI_URL} -o "awscliv2.zip" &&
+    unzip awscliv2.zip &&
+    ./aws/install &&
+    echo "Done installing awscliv2!"
 
-echo "Install graphviz"
-apt-get -qq -y install graphviz
-echo "Done Install graphviz!"
+echo "Install graphviz" &&
+    apt-get -qq -y install graphviz &&
+    echo "Done Install graphviz!"
 
-echo "Install postgresql-client"
-apt-get -qq -y install postgresql-client
-echo "Done Install postgresql-client!"
+echo "Install postgresql-client" &&
+    apt-get -qq -y install postgresql-client &&
+    echo "Done Install postgresql-client!"
 
-echo "Cleaning files!"
-rm -rf /tmp/*
-apt-get -y remove --purge dpkg-dev fakeroot file manpages manpages-dev patch xauth xz-utils
-apt-get -qq -y autoremove
-apt-get -qq clean && apt-get -qq purge
-rm -rf /var/lib/apt/lists/* /var/lib/dpkg/*-old
-rm -rf /usr/share/doc /usr/share/locale/[a-df-z]* /usr/share/locale/e[a-lo-z]* /usr/share/locale/en@* /usr/share/locale/en_GB
-
-echo "Done!"
+echo "Cleaning files!" &&
+    rm -rf /tmp/* &&
+    apt-get -y remove --purge dpkg-dev fakeroot file manpages manpages-dev patch xauth xz-utils &&
+    apt-get -qq -y autoremove &&
+    apt-get -qq clean && apt-get -qq purge &&
+    rm -rf /var/lib/apt/lists/* /var/lib/dpkg/*-old &&
+    rm -rf /usr/share/doc /usr/share/locale/[a-df-z]* /usr/share/locale/e[a-lo-z]* /usr/share/locale/en@* /usr/share/locale/en_GB &&
+    echo "Done!"
