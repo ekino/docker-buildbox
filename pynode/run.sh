8da7520e54563c1d354b47a4257fec320410e3ea
Axel Pavageau <axel.pavageau@ekino.com>
2022-08-05 14:40:11 +0200

    chore: removing awscli install files when finished

--- pynode/run.sh
+++ pynode/run.sh
@@ -13,6 +13,8 @@ echo "Installing AWS CLIv2"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
+rm -f awscliv2.zip
+rm -rf aws
 echo "Done installing awscliv2!"
 
 echo "Install basics Python tools"
